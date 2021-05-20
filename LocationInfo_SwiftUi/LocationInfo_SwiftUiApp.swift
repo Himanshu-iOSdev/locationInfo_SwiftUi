f23f128f819c778d95456370abb43679a0ac49bb
Himanshu Kesharwani <himanshu.kesharwani@quokkalabs.com>
2021-05-20 22:30:28 +0530

    Initial setup

---
+++ LocationInfo_SwiftUi/LocationInfo_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationInfo_SwiftUiApp.swift
+//  LocationInfo_SwiftUi
+//
+//  Created by Himanshu Kesharwani on 20/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LocationInfo_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
